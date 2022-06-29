c6c9de030d13a004a85ad21472b9603c38931824
Gus4090Esp <108242606+Gus4090Esp@users.noreply.github.com>
2022-06-28 21:55:34 -0700

    Initial Commit

---
+++ wham/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  wham
+//
+//  Created by Gustavo Espinoza on 6/28/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
